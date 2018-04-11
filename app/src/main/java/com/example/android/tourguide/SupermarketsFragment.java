8ce54de5d38cd4c1e1c5fb1d08ebaba3a0c5147b
Hosam <hossmnasser938@gmail.com>
2018-04-11 18:02:42 +0200

    Add Fragments classes

---
+++ app/src/main/java/com/example/android/tourguide/SupermarketsFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.tourguide;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Hosam on 4/11/2018.
+ */
+
+public class SupermarketsFragment extends Fragment {
+}
