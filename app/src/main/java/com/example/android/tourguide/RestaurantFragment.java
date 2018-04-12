978b5bda10588fdd34a94a1334bcb53ad21f5491
Hosam <hossmnasser938@gmail.com>
2018-04-12 16:58:51 +0200

    Add Images

--- app/src/main/java/com/example/android/tourguide/RestaurantFragment.java
+++ app/src/main/java/com/example/android/tourguide/RestaurantFragment.java
@@ -26,16 +26,16 @@ public class RestaurantFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.place_list, container, false);
 
         ArrayList<Place> restaurants = new ArrayList<>();
-        restaurants.add(new Place("ElMohamady", "Hasib st", "01026589136"));
-        restaurants.add(new Place("Cook Door", "Adnan ElMalki st", "01113652479"));
-        restaurants.add(new Place("Mcdonald's", "Korneesh st", "01023654125"));
-        restaurants.add(new Place("Bondoka Koshary", "Tah Hussien st", "01125697485"));
-        restaurants.add(new Place("Hi Proust", "Street 14", "01158910237"));
-        restaurants.add(new Place("Elkhaleel", "Korneesh st", "01026794050"));
-        restaurants.add(new Place("KFC", "Korneesh st", "01125301450"));
-        restaurants.add(new Place("Toscanini", "Korneesh st", "01036571000"));
+        restaurants.add(new Place("ElMohamady", "Hasib st", "01026589136", R.mipmap.elmohamadi));
+        restaurants.add(new Place("Cook Door", "Adnan ElMalki st", "01113652479", R.mipmap.cook_door));
+        restaurants.add(new Place("Mcdonald's", "Korneesh st", "01023654125", R.mipmap.mcdonalds));
+        restaurants.add(new Place("Bondoka Koshary", "Tah Hussien st", "01125697485", R.mipmap.bondoka));
+        restaurants.add(new Place("Hi Proust", "Street 14", "01158910237", R.mipmap.hi_proust));
+        restaurants.add(new Place("Elkhaleel", "Korneesh st", "01026794050", R.mipmap.elkhaleel));
+        restaurants.add(new Place("KFC", "Korneesh st", "01125301450", R.mipmap.kfc));
+        restaurants.add(new Place("Toscanini", "Korneesh st", "01036571000", R.mipmap.toscanini));
 
-        PlaceAdapter adapter = new PlaceAdapter(getActivity(), restaurants, false);
+        PlaceAdapter adapter = new PlaceAdapter(getActivity(), restaurants, true);
 
         ListView restaurantsList = rootView.findViewById(R.id.place_list);
 
